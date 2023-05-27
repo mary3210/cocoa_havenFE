fcf22e3be6a25af5b24734c97aef84b9f7d147be
mary3210 <maryrodri3210@gmail.com>
2023-05-27 10:03:27 -0500

    created navbar headers

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='Nav'>Navbar</div>
+    <nav className='nav'>
+    <div className='Header'>
+        <h1>Cocoa Haven</h1>
+        <h2>Login</h2>
+        </div>
+        <div className='nav1'>
+        <h2>Chocolates</h2>
+        <h2>Best Sellers</h2>
+        <h2>About</h2>
+
+    </div>
+    </nav>
   )
 }
