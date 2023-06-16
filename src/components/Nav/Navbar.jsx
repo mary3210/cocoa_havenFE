6a087834864bbd94af4e17241740e32f03fee2be
mary3210 <maryrodri3210@gmail.com>
2023-06-16 16:31:41 -0500

    add css styling to hamburger div

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <nav className='nav'>
-    <div className='Header'>
+    <nav className="nav">
+      <div className="Header">
         <h1>Cocoa Haven</h1>
         <h2>Login</h2>
-        </div>
-        <div className='hamburger'>
-          <div>
-          </div>
-          <div>
-          </div>
-          <div></div>
-        </div>
-        <div className='nav1'>
+      </div>
+      <div className="hamburger">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="nav1">
         <Link to="/chocolates">
-        <p>Chocolates</p>
+          <p>Chocolates</p>
         </Link>
-        <Link to="/bestsellers"> 
-        <p>Best Sellers</p>
+        <Link to="/bestsellers">
+          <p>Best Sellers</p>
         </Link>
         <Link to="/about">
-        <p>About</p>
+          <p>About</p>
         </Link>
-
-    </div>
+      </div>
     </nav>
-  )
+  );
 }
