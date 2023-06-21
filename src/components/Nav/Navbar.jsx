8d36f60c121826a2b0a729970d3c64d47a33484d
unknown <maryrodri3210@gmail.com>
2023-06-20 22:38:36 -0700

    Add transition to hamburger divs

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 export default function Navbar() {
+  function toggleClass(e) {
+    e.currentTarget.classList.toggle("change");
+  }
   return (
     <nav className="nav">
       <div className="Header">
         <h1>Cocoa Haven</h1>
         <h2>Login</h2>
       </div>
-      <div className="hamburger">
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="hamburger" onClick={toggleClass}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
       </div>
       <div className="nav1">
         <Link to="/chocolates">
