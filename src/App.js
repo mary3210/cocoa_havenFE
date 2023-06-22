880c6562aa8fd8770bd18836eda55ca471d3f771
unknown <maryrodri3210@gmail.com>
2023-06-21 20:35:53 -0700

    Created DarkChocolates component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from './components/Nav/home/home';
 import Navbar from './components/Nav/Navbar';
+import MilkChocolates from './components/Nav/MilkChocolates/MilkChocolates';
 import {Routes, Route} from "react-router-dom"
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
      <Navbar/>
      <Routes>
       <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/milkchocolates" element={<MilkChocolates/>}/>
      </Routes>
      
     </div>
