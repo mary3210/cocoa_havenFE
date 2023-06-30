af4581f6b2d9b44b14dc2da12b3bb8f240c7631b
unknown <maryrodri3210@gmail.com>
2023-06-30 15:33:54 -0500

    Added useEffect in PostList

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -4,4 +4,20 @@ import { Link } from "react-router-dom";
 const PostList = () => {
     const [posts, setPosts] = useState([]);
     const BASE_URL = "http:/localhost:8000/"
+
+    const getPosts = async () => {
+        try {
+            const response = await fetch(BASE_URL + "/posts");
+            const allPosts = await response.json();
+            setPosts(allPosts)
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    return 
+
 }
