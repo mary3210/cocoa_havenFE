8281fb8b8f21b1d1f3d87e543a3ab74e5a63fe5c
unknown <maryrodri3210@gmail.com>
2023-06-30 15:30:08 -0500

    Add useState to PostList.jsx

---
+++ src/components/PostList/PostList.jsx
@@ -0,0 +1,7 @@
+import  {useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const PostList = () => {
+    const [posts, setPosts] = useState([]);
+    const BASE_URL = "http:/localhost:8000/"
+}
