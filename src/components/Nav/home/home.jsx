57d96d72d4de870bd3189577c6755115f7f928db
unknown <maryrodri3210@gmail.com>
2023-07-01 16:28:05 -0500

    connected frontend to backend

--- src/components/Nav/home/home.jsx
+++ src/components/Nav/home/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import PostList from '../../PostList/PostList'
 export default function Home() {
   return (
     <div>
         <h3>Home</h3>
         <p>shop now</p>
+        <PostList/>
         </div>
   )
 }
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,23 +1,47 @@
-import  {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const PostList = () => {
-    const [posts, setPosts] = useState([]);
-    const BASE_URL = "http:/localhost:8000/"
+  const [posts, setPosts] = useState([]);
+  const BASE_URL = "http://localhost:8000";
 
-    const getPosts = async () => {
-        try {
-            const response = await fetch(BASE_URL + "/posts");
-            const allPosts = await response.json();
-            setPosts(allPosts)
-        } catch (err) {
-            console.error(err);
-        }
-    };
-    useEffect(() => {
-        getPosts();
-    }, []);
+  const getPosts = async () => {
+    try {
+        console.log(BASE_URL)
+      const response = await fetch(BASE_URL + "/posts");
+      const allPosts = await response.json();
+      console.log(allPosts)
+      setPosts(allPosts);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-    return 
+  return (
+    <>
+      <h2>Chocolist posts</h2>
+      <ul>
+        {posts &&
+          posts.map((post, index) => (
+       
+            <div className="HomePosts" key={index}>
 
-}
+              <Link key={post._id} to={`/post/${post._id}`}>
+                <div className="images">
+                  <img alt={post.tags} src={post.image} />
+                </div>
+                <div className="ptags">
+                  <p>{post.name}</p>
+                </div>
+                
+              </Link>
+            </div>
+          ))}
+      </ul>
+    </>
+  );
+};
+export default PostList
