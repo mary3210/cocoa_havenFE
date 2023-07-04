bec21ea9dbcc7e649cff1538cd51e11164afea70
unknown <maryrodri3210@gmail.com>
2023-07-03 22:48:28 -0500

    Add uste state inapp.js for token login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React, { useState } from 'react';
 import Home from './components/Nav/home/home';
 import Navbar from './components/Nav/Navbar';
 import DarkChocolates from './components/DarkChocolates/DarkChocolates';
 import MilkChocolates from './components/Nav/MilkChocolates/MilkChocolates';
 import {Routes, Route} from "react-router-dom"
 import Login from './components/Login';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 function App() {
+const [token, setToken ] = useState();
 
+if(!token) {
+  return <Login setToken={setToken}/>
+}
   return (
     <div className="App">
       <Login/>
