4e5f4e991a3920e621034e6abf8aea1ac4e2e647
unknown <maryrodri3210@gmail.com>
2023-07-03 23:32:01 -0500

    corrected login to loginform in return

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import './App.css';
 function App() {
 const [token, setToken ] = useState();
 
-// if(!token) {
-//   return <Login setToken={setToken}/>
-// }
+if(!token) {
+  return <LoginForm setToken={setToken}/>
+}
   return (
     <div className="App">
       <Login/>
